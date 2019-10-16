6be37e43530135df45e815ddcff17d2d647cb807
eduardocalix <eduardocalix11xtra@gmail.com>
2019-10-15 20:11:26 -0600

    Creacion de signin

---
+++ Models/modeloPresupuesto.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const slug = require("slug");
+const shortid = require("shortid");
+
+// Definición del schema
+// https://mongoosejs.com/docs/guide.html#models
+// Tipos de schemas en Mongoose
+// https://mongoosejs.com/docs/schematypes.html
+const presupuestoSchema = new mongoose.Schema({
+  ingresoMensual: {
+    type: Number,
+     default: 0,
+    required: "El ingreso mensual es requerido"
+  },
+  fecha: {
+    type: Date,
+    trim: true
+  },
+  mes: {
+    type: String,
+    required: " El mes es requerido",
+    trim: true
+  },
+  salario: {
+    type: String,
+    default: 0,
+    trim: true
+  },
+  contrato: {
+    type: String,
+    trim: true
+  },
+  descripcion: {
+    type: String,
+    trim: true
+  },
+  url: {
+    type: String,
+    lowercase: true
+  },
+  skills: [String],
+  candidatos: [
+    {
+      nombre: String,
+      email: String,
+      hojaVida: String
+    }
+  ]
+});
+// Hooks para generar la URL (en Mongoose se conoce como middleware)
+presupuestoSchema.pre("save", function(next) {
+  // Crear la URL
+  const url = slug(this.titulo);
+  this.url = `${url}-${shortid.generate()}`;
+
+  next();
+});
+
+module.exports = mongoose.model("presupuesto", presupuestoSchema);
